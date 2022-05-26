78aef092420f94cce6f27cd4f9aa6789d8fd2206
Sameer Yaqoob <sameeryaqoob83@gmail.com>
2022-05-26 18:10:30 -0400

    Pushed to Heroku

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require(`${process.env.FIREBASE_PRIVATE_KEY}`);
+const serviceAccount = require(`${process.env.FIREBASE_PRIVATE_KEY_PATH}`);
 const { getFirestore } = require('firebase-admin/firestore');
 
 admin.initializeApp({
